jtlayton/nfs-utils

commit 8b02f0d5590e0610f81bd53aa08155732af3af77

nfsdctl: cleanup listeners if some failed
If we couldn't start some listeners, then make sure to remove
already added listeners.

Signed-off-by: Olga Kornievskaia <okorniev@redhat.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

utils/nfsdctl/nfsdctl.c
@@ -1413,6 +1413,7 @@ static int autostart_func(struct nl_sock *sock, int argc, char ** argv)
 	struct conf_list *thread_str;
 	struct conf_list_node *n;
 	char *scope, *pool_mode;
+	bool failed_listeners = false;
 
 	optind = 1;
 	while ((opt = getopt_long(argc, argv, "h", help_only_options, NULL)) != -1) {
@@ -1447,7 +1448,7 @@ static int autostart_func(struct nl_sock *sock, int argc, char ** argv)
 		return ret;
 	ret = set_listeners(sock);
 	if (ret)
-		return ret;
+		failed_listeners = true;
 
 	grace = conf_get_num("nfsd", "grace-time", 0);
 	lease = conf_get_num("nfsd", "lease-time", 0);
@@ -1455,6 +1456,12 @@ static int autostart_func(struct nl_sock *sock, int argc, char ** argv)
 
 	thread_str = conf_get_list("nfsd", "threads");
 	pools = thread_str ? thread_str->cnt : 1;
+
+	/* if we fail to start one or more listeners, then cleanup by
+	 * starting 0 knfsd threads
+	 */
+	if (failed_listeners)
+		pools = 0;
 
 	threads = calloc(pools, sizeof(int));
 	if (!threads)

commit 3b7de50fe8f59c64762a015693b705db5d0f15e7

nfsdctl: ignore ipv6 listener creation error
If ipv6 is not supported by the kernel and during the nfsd start
we are trying to create an ipv6 listener, ignore the error (this
restores how nfsd handled this failure).

Fixes: 8b02f0d5590e ("nfsdctl: cleanup listeners if some failed")
Signed-off-by: Olga Kornievskaia <okorniev@redhat.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

utils/nfsdctl/nfsdctl.c
@@ -967,6 +967,17 @@ static void print_listeners(void)
 	}
 }
 
+static bool ipv6_is_enabled(void)
+{
+	int s;
+
+	s = socket(AF_INET6, SOCK_STREAM, 0);
+	if (s < 0)
+		return false;
+	close(s);
+	return true;
+}
+
 /*
  * Format is <+/-><netid>:<address>:port
  *
@@ -1081,7 +1092,8 @@ static int update_listeners(const char *str)
 					continue;
 			case AF_INET6:
 				if (r6->sin6_port != l6->sin6_port ||
-				    memcmp(&r6->sin6_addr, &l6->sin6_addr, sizeof(l6->sin6_addr)))
+				    memcmp(&r6->sin6_addr, &l6->sin6_addr, sizeof(l6->sin6_addr)) ||
+				    !ipv6_is_enabled())
 					continue;
 			default:
 
@@ -1127,6 +1139,11 @@ static int set_listeners(struct nl_sock *sock)
 		struct server_socket *sock = &nfsd_sockets[i];
 		struct nlattr *a;
 
+		if (sock->ss.ss_family == AF_INET6 && !ipv6_is_enabled()) {
+			xlog(L_WARNING, "IPv6 isn't enabled, skip IPv6 "
+					"listener\n");
+			continue;
+		}
 		if (sock->ss.ss_family == 0)
 			break;
 