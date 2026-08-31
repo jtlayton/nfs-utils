jtlayton/nfs-utils

commit 6e54f6179cb9e9d5706901a06744b8f4667c24e7

text-based mount.nfs: Add text-based error reporting function
The mount_errors() function prints an error based on what just happened in
the user-space RPC library.  This is meaningless for text-based mounts,
since they don't use the RPC library for most things.

Add a new error printing function that the text-based logic can use to
report an error.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
Signed-off-by: Neil Brown <neilb@suse.de>

utils/mount/error.c
@@ -130,6 +130,61 @@ void rpc_mount_errors(char *server, int will_retry, int bg)
 		fprintf(stderr, "%s\n", errbuf);
 }
 
+/**
+ * sys_mount_errors - log an error that occurred during a mount system call
+ * @server: C string containing name of server we are attempting to mount
+ * @error: errno value to report
+ * @will_retry: one indicates mount will retry at some later point
+ * @bg: one indicates this is a background mount
+ *
+ * Passed an errno value generated by a mount system call, and reports it
+ * on stderr (fg mount) or in the system log (bg mount).
+ */
+void sys_mount_errors(char *server, int error, int will_retry, int bg)
+{
+	int pos = 0;
+	char *tmp;
+	static int onlyonce = 0;
+
+	tmp = &errbuf[pos];
+	if (bg)
+		pos = snprintf(tmp, (erreob - tmp),
+				_("mount to NFS server '%s' failed: "),
+					server);
+	else
+		pos = snprintf(tmp, (erreob - tmp),
+				_("%s: mount to NFS server '%s' failed: "),
+					progname, server);
+
+	tmp = &errbuf[pos];
+	if (error == ETIMEDOUT) {
+		if (will_retry)
+			pos = snprintf(tmp, (erreob - tmp),
+					_("timed out, retrying"));
+		else
+			pos = snprintf(tmp, (erreob - tmp),
+					_("timed out, giving up"));
+	} else {
+		if (bg) {
+			if (will_retry)
+				pos = snprintf(tmp, (erreob - tmp),
+						_("%s, retrying"),
+						strerror(error));
+			else
+				pos = snprintf(tmp, (erreob - tmp),
+						_("%s, giving up"),
+						strerror(error));
+		}
+	}
+
+	if (bg) {
+		if (onlyonce++ < 1)
+			openlog("mount", LOG_CONS|LOG_PID, LOG_AUTH);
+		syslog(LOG_ERR, "%s", errbuf);
+	} else
+		fprintf(stderr, "%s\n", errbuf);
+}
+
 /*
  * mount_error - report a foreground mount error
  * @spec: C string containing the device name being mounted

utils/mount/error.h
@@ -25,4 +25,6 @@ char *nfs_strerror(int);
 
 void mount_error(const char *, const char *, int);
 void rpc_mount_errors(char *, int, int);
+void sys_mount_errors(char *, int, int, int);
+
 void umount_error(int, const char *);

commit 95f26f452a7953ce53abc538fb97e1a5ebbf8b24

mount: fix snprintf return value handling in error formatting
snprintf() returns the number of bytes that would have been written
if sufficient space were available, not the number actually written.
When the formatted string exceeds PATH_MAX (e.g. due to a long
progname), the return value exceeds the buffer size. Using that
unclamped value as an index into errbuf produces an out-of-bounds
pointer, and subsequent snprintf() calls write past the buffer.

Add errbuf_clamp() to bound the position to the valid range before
it is used as a buffer index. Apply it in rpc_mount_errors(),
sys_mount_errors(), and rpc_strerror().

Fixes: 6e54f6179cb9 ("text-based mount.nfs: Add text-based error reporting function")
Signed-off-by: Olga Kornievskaia <okorniev@redhat.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

utils/mount/error.c
@@ -54,6 +54,21 @@ extern char *progname;
 static char errbuf[PATH_MAX];
 static char *erreob = &errbuf[PATH_MAX];
 
+/*
+ * Clamp a buffer position to the valid range for errbuf.
+ * snprintf() returns the number of bytes that would have been written,
+ * which can exceed the buffer size on truncation.  Using that unclamped
+ * value as an index into errbuf produces an out-of-bounds pointer.
+ */
+static int errbuf_clamp(int pos)
+{
+	if (pos < 0)
+		return 0;
+	if (pos >= PATH_MAX)
+		return PATH_MAX - 1;
+	return pos;
+}
+
 /* Convert RPC errors into strings */
 static int rpc_strerror(int spos)
 {
@@ -61,6 +76,8 @@ static int rpc_strerror(int spos)
 	int pos = 0, cf_errno = rpc_createerr.cf_error.re_errno;
 	char *ptr, *estr = clnt_sperrno(cf_stat);
 	char *tmp;
+
+	spos = errbuf_clamp(spos);
 
 	if (estr) {
 		if ((ptr = strchr(estr, ':')))
@@ -72,9 +89,9 @@ static int rpc_strerror(int spos)
 					_("System Error: %s"),
 						strerror(cf_errno));
 		else {
-			if (cf_errno) 
+			if (cf_errno)
 				pos = snprintf(tmp, (erreob - tmp),
-					_("RPC Error:%s; errno = %s"), 
+					_("RPC Error:%s; errno = %s"),
 					estr, strerror(cf_errno));
 			else
 				pos = snprintf(tmp, (erreob - tmp),
@@ -109,6 +126,7 @@ void rpc_mount_errors(char *server, int will_retry, int bg)
 				_("%s: mount to NFS server '%s' failed: "),
 					progname, server);
 
+	pos = errbuf_clamp(pos);
 	tmp = &errbuf[pos];
 	if (rpc_createerr.cf_stat == RPC_TIMEDOUT) {
 		if (will_retry)
@@ -119,6 +137,7 @@ void rpc_mount_errors(char *server, int will_retry, int bg)
 					_("timed out, giving up"));
 	} else {
 		pos += rpc_strerror(pos);
+		pos = errbuf_clamp(pos);
 		tmp = &errbuf[pos];
 		if (bg) {
 			if (will_retry)
@@ -164,6 +183,7 @@ void sys_mount_errors(char *server, int error, int will_retry, int bg)
 				_("%s: mount to NFS server '%s' failed: "),
 					progname, server);
 
+	pos = errbuf_clamp(pos);
 	tmp = &errbuf[pos];
 	if (error == ETIMEDOUT) {
 		if (will_retry)